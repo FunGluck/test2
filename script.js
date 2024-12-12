8d7ab2806810793371c798015ef80420605ff40c
FunGluck <2008alex2000@mail.ru>
2024-12-12 20:05:36 +0500

    fifthlesson

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
-const text = "Привет! Это текст с эффектом печатания.";
-const textBlock = document.getElementsByClassName(".auto-generated-text");
+// const text = "Привет! Это текст с эффектом печатания";
+const textendsymbol = "|"
+const textMassive = ["Привет! Это текст с эффектом печатания", "test2", "test3"]
+const textBlock = document.querySelector(".auto-generated-text");
 let i = 0;
-
-function typeText() {
-    if (i < text.length) {
-        textBlock.innerHTML += text.charAt(i); // добавляем символ по очереди
-        i++;
-    } else {
-        clearInterval(intervalId); // останавливаем интервал, когда весь текст выведен
-    }
+maxSentences = textMassive.length;
+function getRandomSentence() {
+    const index = Math.floor(Math.random() * (maxSentences));
+    return textMassive[index];
 }
+let text = getRandomSentence()
+function typeText() {
+    console.log(text)
+    if (i < text.length) {
+         textBlock.innerHTML += text.charAt(i); // добавляем символ по очереди
+         i++;
+     } else {
+         clearInterval(intervalId); // останавливаем интервал, когда весь текст выведен
+     }
+ }
+ const intervalId = setInterval(typeText, 70); // задержка 100 мс между символами
 
-const intervalId = setInterval(typeText, 100); // задержка 100 мс между символами
+
