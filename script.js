b3298334fc76c9ef820b8408738989c9af8224b5
FunGluck <2008alex2000@mail.ru>
2024-12-19 19:01:07 +0500

    presevenlesson

--- script.js
+++ script.js
@@ -1,32 +1,5 @@
-// const textendsymbol = "|"
-// const textMassive = ["Привет! Это текст с эффектом печатания", "test2", "test3"]
-// const textBlock = document.querySelector(".auto-generated-text");
-// let i = 0;
-// maxSentences = textMassive.length;
-// function getRandomSentence() {
-//     const index = Math.floor(Math.random() * (maxSentences));
-//     return textMassive[index];
-// }
-// let text = getRandomSentence()
-// function typeText() {
-//     console.log(text)
-//     console.log(i)
-//     if (i < text.length) {
-//         textBlock.innerHTML += text.charAt(i); // добавляем символ по очереди
-//         i++;
-//     } else if(i === textBlock.innerHTML.length){
-//         if(textBlock.innerHTML === text + '|') {
-//             textBlock.innerHTML = text
-//         } else {
-//             textBlock.innerHTML += '|'
-//             i++;
-//         }
-//          // clearInterval(intervalId); // останавливаем интервал, когда весь текст выведен
-//     }
-// }
-// const intervalId = setInterval(typeText, 70); // задержка 100 мс между символами
 const textendsymbol = "|";
-const textMassive = ["Привет! Это текст с эффектом печатания", "test2", "test3"];
+const textMassive = ["Работает - не трогай", "Центр Комфорта - город счастья", "Когда ЗП?", "#FreeQuaks", "ChatGPT, помоги...", "/me протянул руку"];
 const textBlock = document.querySelector(".auto-generated-text");
 let i = 0;
 let currentText = "";
